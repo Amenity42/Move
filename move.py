94388d13aedbafe069c420eaa174ba725937ffcf
OddCompost <richomas@hotmail.com>
2022-08-10 14:14:59 +0100

    v1.0.1

--- move.py
+++ move.py
@@ -1,49 +1,46 @@
-from random import random
-from turtle import down
-from typing import Any
 import mouse
+import keyboard
 import time
-
-# Move the mouse to the center of the screen   
-# loop forever
-
-# i=0
-# move_x = 200
-# move_y = 200
-# while i <= 1:
-#     mouse.move(move_x, move_y)
-#     time.sleep(0.2)
-#     mouse.move(move_x, move_y) 
-#     time.sleep(0.2)
-#     mouse.move(move_x, move_y) 
-#     #randomly move the mouse to a new location
-#     move_x = 200 + random() * 100
-#     move_y = 200 + random() * 100 
-#     if(mouse.is_pressed()):
-#         i=1
-    
 
 pressed = False
 mouseRecordedData = []
+key = None
+keyState = None
 
 def recordMouseInput():
     global pressed
     pressed = mouse.is_pressed()
+    mouseRecordedData.append(mouse.get_position())
+    keyboardInput() # call the keyboard input function to get the next key
 
+def playMouseInput():
     global mouseRecordedData
-    if(pressed):
-        for x in mouseRecordedData:
-            mouse.move(x[0], x[1])
-            print(x[0], x[1])
-        print('Finished Replay')
-    else:
-        # pressed = False
-        mouseRecordedData.append(mouse.get_position())
-        print('The mouse is not pressed - Recording')
+    for x in mouseRecordedData:
+        mouse.move(x[0], x[1])
+        print(x[0], x[1])
+    mouseRecordedData = [] # reset the list
+    print('Finished Replay')
+
+def keyboardInput():
+    global key
+    key = keyboard.read_key()
+    #print(key)
+
+    if(key == 'q'):
+        print('Quitting')
+        exit()
+    elif(key == 'r'):
+        print('Recording')
+        recordMouseInput()
+    elif(key == 'm'):
+        print('Replaying')
+        playMouseInput()
+
 
 
 while(True):
     recordMouseInput()
+    keyboardInput()
 
     
 
